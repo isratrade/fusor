ca1cb9994bd1e78a4d4be436542a28874a2cb260
Joseph Magen <jmagen@redhat.com>
2015-09-08 16:33:11 +0300

    remove controllers.deployment.isStarted from hbs

--- fusor-ember-cli/app/controllers/cloudforms/cfme-configuration.js
+++ fusor-ember-cli/app/controllers/cloudforms/cfme-configuration.js
@@ -7,6 +7,8 @@ export default Ember.Controller.extend({
   cfmeRootPassword: Ember.computed.alias("controllers.deployment.model.cfme_root_password"),
   cfmeAdminPassword: Ember.computed.alias("controllers.deployment.model.cfme_admin_password"),
   isSubscriptions: Ember.computed.alias("controllers.deployment.isSubscriptions"),
+  isStarted: Ember.computed.alias("controllers.deployment.isStarted"),
+  notValidCloudforms: Ember.computed.alias("controllers.cloudforms.notValidCloudforms"),
 
   nextRouteNameAfterCFME: function() {
     if (this.get('isSubscriptions')) {
