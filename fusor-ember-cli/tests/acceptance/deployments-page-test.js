ce4b1211100011893a39dd6eed717ea9c0d884b9
Joseph Magen <jmagen@redhat.com>
2015-09-08 12:58:59 +0300

    wrapped Ember.run

--- fusor-ember-cli/tests/acceptance/deployments-page-test.js
+++ fusor-ember-cli/tests/acceptance/deployments-page-test.js
@@ -58,11 +58,52 @@ test('display rows of deployments', function(assert) {
   });
 });
 
-// test('user filters list of deployments', function(assert) {
+test('user filters list of deployments', function(assert) {
+  visit('/deployments');
+  fillIn('input.filter-input', 'a');
+
+  andThen(function() {
+    var deployments = find('tr.deployment-row');
+    assert.equal(deployments.length, 1);
+  });
+
+  fillIn('input.filter-input', 't');
+
+  andThen(function() {
+    var deployments = find('tr.deployment-row');
+    assert.equal(deployments.length, 2);
+  });
+
+});
+
+//get error Assertion Failed: You have turned on testing mode, which disabled the run-loop's autorun. You will need to wrap any code with asynchronous side-effects in a run
+// test('click edit button for deployment on deployments page', function(assert) {
 //   visit('/deployments');
-//   fillIn('input.filter-input', 'aaa');
 
 //   andThen(function() {
+//     click('tr.deployment-row:first-child > td:last-child > a');
 //     assert.equal(currentURL(), '/deployments/new/start');
 //   });
 // });
+
+test('edit deployment page', function(assert) {
+    visit('/deployments/1/satellite');
+
+    andThen(function() {
+          assert.equal(currentURL(), '/deployments/1/satellite');
+          assert.equal($.trim(find('h1').text()), 'New RHCI Deployment:  aaa');
+      // assert.equal($.trim(find('.new-deployment-button').text()), 'New Deployment');
+      // assert.equal(find('.new-deployment-button a').attr('href'), '#/deployments/new/start');
+      // assert.equal($.trim(find('.form-group button').text()), 'Search');
+      // assert.equal($.trim(find('.filter-deployments input').attr('placeholder')), 'Filter ...');
+      // assert.ok(find('table.deployments-table').length);
+      // assert.equal($.trim(find('table.deployments-table > thead > tr > th:nth-child(1)').text()), 'Name');
+      // assert.equal($.trim(find('table.deployments-table > thead > tr > th:nth-child(2)').text()), 'Environment');
+      // assert.equal($.trim(find('table.deployments-table > thead > tr > th:nth-child(3)').text()), 'Organization');
+      // assert.equal($.trim(find('table.deployments-table > thead > tr > th:nth-child(4)').text()), 'Status');
+      // var displayingString = $.trim(find('.displaying-entries').text());
+      // assert.ok(new RegExp('Displaying').test(displayingString), 'Should show text Displaying ## of ## entries');
+      // // assert.ok(new RegExp('^displaying\s+\d+\s+of\s+\d+\s+entries$').test(displayingString), 'Should show text Displaying ## of ## entries');
+      // // assert.ok(new RegExp('^displaying\s\d').test('displaying'));
+    });
+});
