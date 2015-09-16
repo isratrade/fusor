711f871365bf900a02d3014b8fe09ce46faf79bd
Joseph Magen <jmagen@redhat.com>
2015-09-16 15:31:44 +0300

    add openshift to UI

---
+++ fusor-ember-cli/tests/unit/routes/openshift/openshift-masters-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:openshift/openshift-masters', 'Unit | Route | openshift/openshift masters', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  var route = this.subject();
+  assert.ok(route);
+});
