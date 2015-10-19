367fbb466c2a7a14f1b12c1f3a4ee9f3f15bb6bc
Joseph Magen <jmagen@redhat.com>
2015-10-19 13:42:13 +0300

    enable API server mocks for RHEV and OSP

--- fusor-ember-cli/server/mocks/deployment-plan.js
+++ fusor-ember-cli/server/mocks/deployment-plan.js
@@ -521,6 +521,6 @@ module.exports = function(app) {
     });
   });
 
-  app.use('/fusor/api222/openstack/deployments', deploymentPlanRouter);
+  app.use('/fusor/api/openstack/deployments', deploymentPlanRouter);
 
 };
