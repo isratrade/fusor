6b32dd8b3b5c19455d5225ad7e12397daec5e44a
Joseph Magen <jmagen@redhat.com>
2015-11-08 22:17:27 +0200

    refactored ember-data adapters hoook buildURL to urlForQuery

--- fusor-ember-cli/app/adapters/node.js
+++ fusor-ember-cli/app/adapters/node.js
@@ -2,11 +2,9 @@ import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
 
-    buildURL: function (type, query) {
-        console.log('query  ddddddddd ');
-        console.log(query);
-
+    urlForQuery: function (query, modelName) {
         return '/fusor/api/openstack/deployments/' + query['deployment_id'] + '/nodes';
     }
+
 
 });
