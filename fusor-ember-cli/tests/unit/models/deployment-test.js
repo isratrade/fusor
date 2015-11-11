008912c37d81272ae0c7c9ad298501b6c8189864
Joseph Magen <jmagen@redhat.com>
2015-11-11 14:06:28 +0200

    first test on Unit | Model | deployment

--- fusor-ember-cli/tests/unit/models/deployment-test.js
+++ fusor-ember-cli/tests/unit/models/deployment-test.js
@@ -1,12 +1,37 @@
 import { moduleForModel, test } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleForModel('deployment', 'Unit | Model | deployment', {
   // Specify the other units that are required for this test.
-  needs: ['model:organization', 'model:lifecycle-environment', 'model:discovered-host', 'model:subscription']
+  needs: ['model:organization',
+          'model:lifecycle-environment',
+          'model:discovered-host',
+          'model:subscription',
+          'model:introspection-task',
+          'model:foreman-task',
+          'adapter:application'
+         ]
 });
 
 test('it exists', function(assert) {
   var model = this.subject();
   // var store = this.store();
   assert.ok(!!model);
+});
+
+test('isStarted', function(assert){
+  var model = this.subject();
+  assert.equal(model.get('isStarted'), false);
+
+  Ember.run(function() {
+    model.set('foreman_task_uuid', 'db25a76f-e344-48ba-ac77-f29303586dbe');
+  });
+
+  assert.equal(model.get('isStarted'), true);
+
+  Ember.run(function() {
+    model.set('foreman_task_uuid', null);
+  });
+
+  assert.equal(model.get('isStarted'), false);
 });
