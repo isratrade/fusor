f82075880dbdc63ec6c78d996cf34f66541c9b06
Joseph Magen <jmagen@redhat.com>
2015-12-21 17:58:32 +0200

    change deployment and foreman-task to ActiveModelSerializer

--- fusor-ember-cli/app/serializers/deployment.js
+++ fusor-ember-cli/app/serializers/deployment.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
-export default DS.RESTSerializer.extend({
+export default DS.ActiveModelSerializer.extend({
   isNewSerializerAPI: true,
   attrs: {
      foreman_task: {key: 'foreman_task_uuid'}
--- fusor-ember-cli/app/serializers/foreman-task.js
+++ fusor-ember-cli/app/serializers/foreman-task.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
-export default DS.RESTSerializer.extend({
+export default DS.ActiveModelSerializer.extend({
+  isNewSerializerAPI: true,
   attrs: {
     humanized: { embedded: 'always' }
   }
