54fea6a24eab8f84c766d12b77bc1ed0b7d1d2c8
Joseph Magen <jmagen@redhat.com>
2016-04-21 13:16:55 +0300

    squash this commit

--- server/app/models/setting/cloudforms.rb
+++ server/app/models/setting/cloudforms.rb
@@ -10,10 +10,14 @@ module Fusor
           self.set('cloudforms_db_disk_size', N_("Size of Storage (GB) for DB for CloudForms"), 40),
           self.set('cloudforms_ram', N_("Amount of RAM (GB) for CloudForms"), 6),
 <<<<<<< HEAD
+<<<<<<< HEAD
           self.set('cloudforms_vcpu', N_("Number of vCPU's for CloudForms"), 4)
 =======
           self.set('cloudforms_vcpu', N_("Number of vCPU's for CloudForms"), 4),
 >>>>>>> 8228254... Add default settings for CFME and OSE VMs editable by Satellite6
+=======
+          self.set('cloudforms_vcpu', N_("Number of vCPU's for CloudForms"), 4)
+>>>>>>> 9d4c2be... rubocop: whitespace, indentation.
         ].each { |s| self.create! s.update(:category => "Setting::Cloudforms") }
 
       end
