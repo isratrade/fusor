fusor/welder

Added subnet check for floating gateway IP

fusor-ember-cli/app/controllers/openstack/overcloud.js
@@ -78,7 +78,10 @@ let OvercloudController = Ember.Controller.extend(DeploymentControllerMixin, Nee
   }),
 
   isValidFloatingIpGateway: Ember.computed('openstackOvercloudFloatGateway', function () {
-    return this.get('ipValidator').isValid(this.get('openstackOvercloudFloatGateway'));
+    var network = new Address4(this.get('openstackOvercloudFloatGateway'));
+    var isInSubnet = network.isInSubnet(this.get('openstackOvercloudFloatGateway'));
+
+    return this.get('ipValidator').isValid(this.get('openstackOvercloudFloatGateway')) && isInSubnet;
   })
 });
 

isratrade/fusor

fusor-ember-cli/app/controllers/openstack/overcloud.js
@@ -55,7 +55,10 @@ const OvercloudController = Ember.Controller.extend(DeploymentControllerMixin, N
   }),
 
   isValidFloatingIpGateway: Ember.computed('overcloudFloatGateway', function () {
-    return this.get('ipValidator').isValid(this.get('overcloudFloatGateway'));
+    var network = new Address4(this.get('overcloudFloatGateway'));
+    var isInSubnet = network.isInSubnet(this.get('overcloudFloatGateway'));
+
+    return this.get('overcloudFloatGatewayValidator').isValid(this.get('overcloudFloatGateway')) && isInSubnet;
   })
 });
 