b2c6daa8c57d0e8d749453be69cf8bec3c712624
Joseph Magen <jmagen@redhat.com>
2016-05-19 13:48:52 +0300

    squash this commit

--- server/app/controllers/fusor/api/v3/organizations_controller.rb
+++ server/app/controllers/fusor/api/v3/organizations_controller.rb
@@ -11,16 +11,27 @@
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
 module Fusor
-  class Api::V3::OrganizationsController < ::Api::V2::OrganizationsController
+  class Api::V3::OrganizationsController < Api::V3::BaseController
 
     #include Api::Version3
 
+    before_filter :find_organization, :only => [:show, :update, :destroy]
+
     def index
+      @organizations = ::Organization.all
       render :json => @organizations, :each_serializer => OrganizationSerializer
     end
 
     def show
       render :json => @organization, :serializer => OrganizationSerializer
+    end
+
+    private
+
+    def find_organization
+      @organization = ::Organization.find_by_id(params[:organization_id])
+      return @organization if @organization
+      return :json => {:error => "Couldn't find organization" }
     end
 
   end
