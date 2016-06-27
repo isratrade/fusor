bc3f6385f387dbac6dee9834c9eae5484afd78e3
Joseph Magen <jmagen@redhat.com>
2016-06-27 15:05:13 +0300

    add ember-ic-you

--- fusor-ember-cli/app/routes/deployments.js
+++ fusor-ember-cli/app/routes/deployments.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
+import InfiniteScrollRouteMixin from 'ember-cli-infinite-scroll/mixins/infinite-scroll-route';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(InfiniteScrollRouteMixin, {
   model() {
-    return this.store.findAll('deployment');
+//    return this.store.findAll('deployment');
+    return this.infiniteQuery('deployment');
   },
 
   actions: {
