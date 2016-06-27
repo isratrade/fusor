0c9ddaf2c54eeed5caa650e84cf5400c0f1b5e5d
Joseph Magen <jmagen@redhat.com>
2016-06-27 17:39:24 +0300

    add infinite scroll pagination to deployments list rather than retrieving all

--- fusor-ember-cli/app/mirage/config.js
+++ fusor-ember-cli/app/mirage/config.js
@@ -7,7 +7,15 @@ export default function() {
     return {};
   });
 
-  this.get('/fusor/api/v21/deployments');
+  this.get('/fusor/api/v21/deployments', function(db, request) {
+    return {deployments: db.deployments,
+            meta: {
+              total: 107,
+              total_pages: 5
+            }
+           };
+  });
+
   this.post('/fusor/api/v21/deployments');
   this.get('/fusor/api/v21/deployments/:id');
   this.put('/fusor/api/v21/deployments/:id');
