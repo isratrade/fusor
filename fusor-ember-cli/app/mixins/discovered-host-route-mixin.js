13ae6a6ff41896264d79ad6c1d171e7f8dc1a1a6
Joseph Magen <jmagen@redhat.com>
2016-07-02 22:44:08 +0300

    squash this commit

--- fusor-ember-cli/app/mixins/discovered-host-route-mixin.js
+++ fusor-ember-cli/app/mixins/discovered-host-route-mixin.js
@@ -2,12 +2,33 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
 
+  queryParams: {
+    search: {
+      refreshModel: true
+    },
+    page: {
+      refreshModel: true
+    },
+    sort_by: {
+      refreshModel: true
+    },
+    dir: {
+      refreshModel: true
+    }
+  },
+
   setupController(controller, model) {
     controller.set('model', model);
     if (this.modelFor('deployment').get('isNotStarted')) {
       controller.set('isLoadingHosts', true);
-      this.store.findAll('discovered-host').then(function(results) {
+      this.store.query('discovered-host', params).then(function(results) {
+        console.log('3333333333333333333');
+        alert(results);
         controller.set('allDiscoveredHosts', results.filterBy('is_discovered', true));
+        controller.set('totalDiscoveredHosts', results.get('meta.total'));
+        controller.set('pageNumber', results.get('meta.page'));
+        controller.set('totalPages', results.get('meta.total_pages'));
+        controller.set('pageRange', _.range(1, results.get('meta.total_pages') + 1));
         controller.set('isLoadingHosts', false);
       });
     }
