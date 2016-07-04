ad6ca203319515ff0fb0adac953c0ce597df8500
Joseph Magen <jmagen@redhat.com>
2016-07-04 13:40:02 +0300

    squash this commit

--- fusor-ember-cli/app/controllers/hypervisor/discovered-host.js
+++ fusor-ember-cli/app/controllers/hypervisor/discovered-host.js
@@ -134,49 +134,6 @@ export default Ember.Controller.extend(NeedsDeploymentMixin, {
     ]
   }),
 
-  prevPage: Ember.computed('pageNumber', function() {
-    return parseInt(this.get('pageNumber')) - 1;
-  }),
-
-  nextPage: Ember.computed('nextPage', function() {
-    return parseInt(this.get('pageNumber')) + 1;
-  }),
-
-  disablePrevPage: Ember.computed('pageNumber', function() {
-    return parseInt(this.get('pageNumber')) === 1 || Ember.isBlank(this.get('pageNumber'));
-  }),
-
-  disableNextPage: Ember.computed('pageNumber', 'totalPages', function() {
-    return parseInt(this.get('pageNumber')) === parseInt(this.get('totalPages'));
-  }),
-
-  entriesFrom: Ember.computed('pageNumber', 'totalPages', 'totalDeployments', function() {
-    return ((parseInt(this.get('pageNumber')) * 20) - 19);
-  }),
-
-  entriesTo: Ember.computed('pageNumber', 'totalPages', 'totalDeployments', function() {
-    if (parseInt(this.get('pageNumber')) === parseInt(this.get('totalPages'))) {
-      return this.get('totalDeployments');
-    } else {
-      return (parseInt(this.get('pageNumber')) * 20);
-    }
-  }),
-
-  showPagination: Ember.computed('totalPages', function() {
-    return (parseInt(this.get('totalPages')) > 1);
-  }),
-
-  displayingEntries: Ember.computed('totalDeployments', 'totalPages', 'entriesFrom', 'entriesTo', function() {
-    if (parseInt(this.get('totalDeployments') === 0)) {
-      return 'No entries found';
-    } else if (parseInt(this.get('totalPages')) < 2) {
-      return `Displaying <strong>all ${this.get('totalDeployments')}</strong> entries`.htmlSafe();
-    } else {
-      return `Displaying entries <strong>${this.get('entriesFrom')} - ${this.get('entriesTo')}</strong> of <strong>${this.get('totalDeployments')}</strong> in total`.htmlSafe();
-    }
-  }),
-
-
   actions: {
 
     setCheckAll() {
