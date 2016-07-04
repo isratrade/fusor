73a790b41dceb2e9c2d4228997884226eb4bc864
Joseph Magen <jmagen@redhat.com>
2016-07-04 14:52:21 +0300

    squash this commit

--- fusor-ember-cli/app/mixins/discovered-host-route-mixin.js
+++ fusor-ember-cli/app/mixins/discovered-host-route-mixin.js
@@ -17,14 +17,34 @@ export default Ember.Mixin.create({
     }
   },
 
+  getDiscoveredHosts(params) {
+    // note, this function does NOT set isLoadingHosts to true and then false
+    // otherwise, it would show the loading spinner for each search and sort
+    var controller = this.controllerFor('engine/discovered_host');
+    if (this.modelFor('deployment').get('isNotStarted')) {
+      let sort_by = params['sort_by'] || 'name';
+      let dir = params['dir'] || 'ASC';
+      let page = params['page'] || 1;
+      params['order'] = sort_by + ' ' + dir;
+      return this.store.query('discovered-host', params).then(function(results) {
+        controller.set('allDiscoveredHosts', results.filterBy('is_discovered', true));
+        controller.set('totalDiscoveredHosts', results.get('meta.total'));
+        controller.set('pageNumber', results.get('meta.page'));
+        controller.set('totalPages', results.get('meta.total_pages'));
+        controller.set('pageRange', _.range(1, results.get('meta.total_pages') + 1));
+      });
+    }
+  },
+
   actions: {
     refreshDiscoveredHosts() {
       console.log('refresh allDiscoveredHosts');
-      var self = this;
-      var controller = this.controllerFor('engine/discovered_host');
+      let controller = this.get('controller');
       controller.set('isLoadingHosts', true);
-      this.store.query('discovered-host', {}).then(function(results) {
+      this.store.query('discovered-host', {page: 1, search: ''}).then(function(results) {
         controller.set('isLoadingHosts', false);
+        controller.set('page', 1);
+        controller.set('search', '');
         controller.set('allDiscoveredHosts', results.filterBy('is_discovered', true));
         controller.set('totalDiscoveredHosts', results.get('meta.total'));
         controller.set('pageNumber', results.get('meta.page'));
--- fusor-ember-cli/app/routes/engine/discovered-host.js
+++ fusor-ember-cli/app/routes/engine/discovered-host.js
@@ -27,25 +27,6 @@ export default Ember.Route.extend(DiscoveredHostRouteMixin, {
     });
   },
 
-  getDiscoveredHosts(params) {
-    var controller = this.controllerFor('engine/discovered_host');
-    if (this.modelFor('deployment').get('isNotStarted')) {
-      let sort_by = params['sort_by'] || 'name';
-      let dir = params['dir'] || 'ASC';
-      let page = params['page'] || 1;
-      params['order'] = sort_by + ' ' + dir;
-      controller.set('isLoadingHosts', true);
-      return this.store.query('discovered-host', params).then(function(results) {
-        controller.set('allDiscoveredHosts', results.filterBy('is_discovered', true));
-        controller.set('totalDiscoveredHosts', results.get('meta.total'));
-        controller.set('pageNumber', results.get('meta.page'));
-        controller.set('totalPages', results.get('meta.total_pages'));
-        controller.set('pageRange', _.range(1, results.get('meta.total_pages') + 1));
-        controller.set('isLoadingHosts', false);
-      });
-    }
-  },
-
   deactivate() {
     return this.send('saveDeployment', null);
   }
