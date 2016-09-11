9d62daf6a026b05691cf1333598a50460c8e9db0
Joseph Magen <jmagen@redhat.com>
2016-09-11 23:14:04 +0300

    event-bus changes to initializer

--- fusor-ember-cli/tests/unit/initializers/event-bus-test.js
+++ fusor-ember-cli/tests/unit/initializers/event-bus-test.js
@@ -10,26 +10,25 @@ module('Unit | Initializer | event bus', {
   beforeEach() {
     Ember.run(function() {
       application = Ember.Application.create();
-      reg = application.registry;
       application.deferReadiness();
     });
   }
 });
 
-// test('app instance is non-null', function(assert) {
-//   EventBusInitializer.initialize(reg, application);
-//   assert.ok(application);
-// });
+test('app instance is non-null', function(assert) {
+  EventBusInitializer.initialize(application);
+  assert.ok(application);
+});
 
 // test('event-bus registration', function(assert) {
-//   EventBusInitializer.initialize(application.registry, application);
+//   EventBusInitializer.initialize(application);
 //   assert.ok(reg['registrations'][factoryName]);
 // });
 
 // test('event-bus injection', function(assert) {
 //   assert.expect(4);
 
-//   EventBusInitializer.initialize(application.registry, application);
+//   EventBusInitializer.initialize(application);
 
 //   let assertInjection = (target) => {
 //     assert.ok(reg['_typeInjections'][target].find((type) => {
