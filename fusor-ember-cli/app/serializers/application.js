f0ecb503397794f885e6ba24773052e2d28b3a56
Joseph Magen <jmagen@redhat.com>
2016-09-11 21:56:37 +0300

    serailzers

--- fusor-ember-cli/app/serializers/application.js
+++ fusor-ember-cli/app/serializers/application.js
@@ -1,7 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPISerializer.extend({
-  isNewSerializerAPI: true,
 
   // disable dasherize transformation
   keyForAttribute: function(attr) {
