87a640b2abdb7b67ca0c3465010bc6432f5c160a
Joseph Magen <jmagen@redhat.com>
2016-09-29 17:55:18 +0300

    upgrade Ember and Ember-Data version to LTS (Long Term Support) Release, version 2.4.x

--- fusor-ember-cli/app/serializers/application.js
+++ fusor-ember-cli/app/serializers/application.js
@@ -2,8 +2,6 @@ import DS from 'ember-data';
 import { ActiveModelSerializer } from 'active-model-adapter';
 
 export default ActiveModelSerializer.extend({
-  isNewSerializerAPI: true,
-
   attrs: {
     foreman_task_id: false
   }
